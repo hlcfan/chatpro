305f176171b94f2254a8c9d72f4349b5545b714b
Shi Yan <hlcfan.yan@gmail.com>
2012-03-26 15:34:37 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatpro::Application.config.secret_token = 'e14b15edc79e481ebe5385a0afef5a5d26bc14dff2d73931c53d0aa8697261268811c6ac217dcaa3cfa8ccdcbb35e24df1e95e56eef9341bc13cb38553e74274'
