b906690eaa0c1359d21cccee4590178b1ed2a828
Shi Yan <hlcfan.yan@gmail.com>
2012-04-10 11:44:31 +0800

    fix scrollbar&juggernaut loading&input box

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,14 +15,20 @@ class UsersController < ApplicationController
   
   def update
   	@user = User.find params[:id]
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to(@user, :notice => 'Profile was successfully updated.') }
-        format.json  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.json  { render :json => @user.errors, :status => :unprocessable_entity }
-      end
+    @user.weibo_id = params[:user][:weibo_id]
+    @user.website = params[:user][:website]
+    @user.email = params[:user][:email]
+    @user.facebook_id = params[:user][:facebook_id]
+    @user.twitter_id = params[:user][:twitter_id]
+    @user.github_id = params[:user][:github_id]
+    @user.googleplus_id = params[:user][:googleplus_id]
+    @user.stackoverflow_id = params[:user][:stackoverflow_id]
+    @user.intro = params[:user][:intro]
+    if @user.save
+      redirect_to(@user, :notice => 'Profile was successfully updated.')
+    else
+      render :action => :edit
     end
   end
 end
+
