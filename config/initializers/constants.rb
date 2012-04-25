007c189a2fa9ec2783c27fb8d6e88e4398be74ab
Shi Yan <hlcfan.yan@gmail.com>
2012-04-25 09:18:55 +0800

    Juggernaut Server Const

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,11 @@
 # coding: utf-8
 require "socket"
-JUGGERNAUT_SERVER = UDPSocket.open {|s| s.connect("64.233.187.99", 1); s.addr.last}
+
 WillPaginate::ViewHelpers.pagination_options[:previous_label] = "新一点"
 WillPaginate::ViewHelpers.pagination_options[:next_label] = "旧一点"
+case ENV['RAILS_ENV']  
+  when "development"
+    JUGGERNAUT_SERVER = UDPSocket.open {|s| s.connect("64.233.187.99", 1); s.addr.last}
+  when "production"  
+    JUGGERNAUT_SERVER = "106.187.94.74"
+end
