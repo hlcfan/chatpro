aae73bb25945b032cd9cdefe6f938f50db2abdde
Shi Yan <hlcfan.yan@gmail.com>
2013-05-27 20:27:26 +0800

    x

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,8 @@ module Chatpro
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    config.assets.compile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
